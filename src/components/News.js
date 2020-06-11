64965d590c1d145a0555734d04699e6ea3bbe047
Rohithgilla12 <gillarohith1@gmail.com>
2020-06-11 18:14:07 +0530

    Added news

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,10 @@ import {
   List,
   ListItem,
   ListSubheader,
-  Divider
+  Divider,
 } from "@material-ui/core";
 
-const Content = props => {
+const Content = (props) => {
   return (
     <ListItem>
       <Grid item xs={12}>
@@ -19,12 +19,13 @@ const Content = props => {
 };
 
 const newsArr = [
-  "Dr. S Jagan Mohan Rao successfully defended his doctoral thesis",
+  "Our recent work is highlighted by JPD in twitter, https://twitter.com/JPhysD/status/1269736008008032258",
+  "Dr. S Jagan Mohan Rao successfully defended his doctoral thesis, March 2020",
   "Review article on Terahertz Metamaterials is published by Journal of Electromagnetic Materials and Waves in January 2020 [January 2020]",
   "Prof. Michael feiginov from TU Vienna visited for one week [December 2019]",
   "Mr. Sabyasachi Banerjee received best poster award in BITS Pilani Hyderabad PhD presentation symposium [November 2019]",
   "Our work (Deep Sub-wavelength Coupling Induced Fano Resonances in Symmetric Terahertz Metamaterials) highlighted in the cover page of Physica Status Solidi: Rapid Research Letters October, 2019 ",
-  "Mr. Subhajit Karmakar received best poster award in ICOL 2019 held in Dehradun for Fano resonant Metamaterials [September, 2019]"
+  "Mr. Subhajit Karmakar received best poster award in ICOL 2019 held in Dehradun for Fano resonant Metamaterials [September, 2019]",
 ];
 
 const News = () => {
@@ -35,7 +36,7 @@ const News = () => {
           <ListSubheader>News</ListSubheader>
           {/*  eslint-disable-next-line */}
           <marquee direction="down" scrollamount="2">
-            {newsArr.map(element => {
+            {newsArr.map((element) => {
               return <Content data={element} />;
             })}
           </marquee>
